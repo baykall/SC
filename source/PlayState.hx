638352e7f088cb15669607cbb3c314e51ef02f5c
baykall <baykal@gmail.com>
2015-03-09 04:22:24 -0400

    Farm house addition and several small updates

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -19,6 +19,10 @@ enum State {
 	Paused;
 	Running;
 }
+enum BuildMode{
+	None;
+	WheatFarm;
+}
 
 class PlayState extends FlxState{
 	// Static variables
@@ -34,11 +38,14 @@ class PlayState extends FlxState{
 	private var terrain:FlxTilemap;
 	private var plants:FlxTilemap;
 	private var roads:FlxTilemap;	
-	private var towns:FlxTilemap;	
-	private var terrain_map:String;	
-	private var plants_map:String;	
-	private var roads_map:String;	
-	private var towns_map:String;	
+	private var towns:FlxTilemap;
+	private var buildings:FlxTilemap;	
+
+	private var mouseicon:FlxSprite;
+
+
+// Load the sprite's graphic to the cursor
+//FlxG.mouse.load(sprite.pixels);	
 		
 	// Camera
 	private var camera:FlxCamera;
@@ -59,18 +66,27 @@ class PlayState extends FlxState{
 	private var resourcesButton:FlxButton;	
 	private var optionsButton:FlxButton;
 	
-	// Game engine
-	private var time:Time;
-	private var state:State;
+	// Game status
+	private var state:State = Running;	
+	private var buildMode:BuildMode = None;
+	private var stateSwitch:Bool = false;
+	private var buildModeSwitch:Bool = false;	
 
 	// Game entities
+	private var time:Time;	
 	private var trucks:Array<FlxSprite>;
 	private var truck_paths:Array<FlxPath>;
 	private var cities:Array<City>;	
 	private var city_labels:Array<FlxText>;	
 
+	private var truck_def_speed:Float;
+
+	// Main Functions
+
 	override public function create():Void{
 		super.create();
+
+		init_parameters();
 		
 		init_time();	
 		init_cities();	
@@ -93,133 +109,102 @@ class PlayState extends FlxState{
 		// Game handling
 		move_camera();
 		bound_camera();
+
+		// Inputs from keyboard and mouse
 		input_keyboard();
+		input_mouse();
 
-		// Time update
-		if(state == Running)
+		// Update items dependent on the inputs
+		update_state_dependent_items();
+
+		if(state == Running){
 			update_time();
-		
-		// Game features update
-		move_trucks();
-		remove_arrived_trucks();
-		if(time.moment == NewDay)
-			execute_todays_events();
-
-	}
-
-	public function execute_todays_events(){
-		if(time.total_days==2)
-			ship_truck(26,34,16,6);
-		if(time.total_days==3)
-			ship_truck(30,19,15,20);
-		if(time.total_days==4)
-			ship_truck(30,19,15,20);			
-	}
-
-	public function move_trucks(){
-		for(i in 0...trucks.length){
-			if (!truck_paths[i].finished && truck_paths[i].nodes!=null) {
-				if (truck_paths[i].angle == 0)
-					trucks[i].animation.play("up");
-				else if (truck_paths[i].angle == 180) 
-					trucks[i].animation.play("down");
-				else if (truck_paths[i].angle == 90) 
-					trucks[i].animation.play("right");
-				else if (truck_paths[i].angle == -90) 
-					trucks[i].animation.play("left");
-			} 
-			else{
-				trucks[i].animation.curAnim.curFrame = 0;
-				trucks[i].animation.curAnim.stop();
-			}
-		}		
-	}
-
-	public function remove_arrived_trucks(){
-		var truck_no:Int;
-
-		truck_no = -1;
-		for(i in 0...trucks.length){	
-			if(truck_paths[i].finished){
-				remove(trucks[i]);		
-				truck_no = i;	
-			}
+			move_trucks();
+			remove_arrived_trucks();
+			if(time.moment == NewDay)
+				execute_todays_events();
 		}
+	}
 
-		if(truck_no >= 0){
-			trucks.splice(truck_no,1);
-			truck_paths.splice(truck_no,1);
-		}
+	// Initialization Functions
+
+	public function init_parameters(){
+
+		truck_def_speed = 150;
+	}
+
+	public function init_time(){
+		time = new Time(12, "January", 1678, 100);
+		state = Running;	
 	}	
 
-	public function ship_truck(from_x,from_y,to_x,to_y){
+	public function init_cities(){
+		cities = new Array<City>();
 
-		var truck:FlxSprite;
-		var truck_path:FlxPath;
+		var city:City;
 
-		truck = new FlxSprite((from_x - 1) * TILE_WIDTH, (from_y - 1) * TILE_HEIGHT);
-		truck.loadGraphic("assets/images/truck.png", true, TILE_WIDTH, TILE_HEIGHT);
-		truck.animation.add("left", [0]);	
-		truck.animation.add("right", [1]);			
-		truck.animation.add("down", [2]);
-		truck.animation.add("up", [3]);		
-		
-		trucks.push(truck);
-
-		add(trucks[trucks.length - 1]);
-
-		truck_path = new FlxPath();	
-		truck_paths.push(truck_path);	
-	
-		var nodes:Array<FlxPoint> = roads.findPath(FlxPoint.get(truck.x, truck.y), FlxPoint.get((to_x - 0.5) * TILE_WIDTH, (to_y - 0.5) * TILE_HEIGHT));
-
-		if (nodes != null) 
-			truck_paths[truck_paths.length - 1].start(trucks[trucks.length - 1], nodes);
-
-	}
-
-	public function init_trucks(){
-		trucks = new Array<FlxSprite>();
-		truck_paths = new Array<FlxPath>();
-	}
+		city = new City("Ann Arbor", 14, 6, [500,200,50]);
+		cities.push(city);	
+		city = new City("Atlanta", 33, 19, [400,300,50]);
+		cities.push(city);	
+		city = new City("New York", 12, 20, [1000,400,0]);
+		cities.push(city);	
+		city = new City("Phoenix", 14, 34, [800,100,25]);
+		cities.push(city);			
+	}		
 
 	public function init_graphics(){
 		city_labels = new Array<FlxText>();	
 		var label:FlxText;
 
+		var terrain_map:String;	
+		var plants_map:String;	
+		var roads_map:String;	
+		var towns_map:String;
+		var buildings_map:String;	
+
 		terrain_map = Assets.getText("assets/data/terrain_map.csv");
 		plants_map = Assets.getText("assets/data/resources_map.csv");
 		roads_map = Assets.getText("assets/data/roads_map.csv");
-		towns_map = get_cities_map();
+		towns_map = get_map("Cities");
+		buildings_map = get_map("Buildings");		
 
+		// Terrain
 		terrain = new FlxTilemap();
 		terrain.loadMap(terrain_map, "assets/images/terrain.png", TILE_WIDTH, TILE_HEIGHT, 0, 1);	
 		add(terrain);	
 	
+		// Plants
 		plants = new FlxTilemap();
 		plants.loadMap(plants_map, "assets/images/resources.png", TILE_WIDTH, TILE_HEIGHT, 0, 1);				
 		add(plants);
 
+		// Roads
 		roads = new FlxTilemap();
 		roads.loadMap(roads_map, "assets/images/roads.png", TILE_WIDTH, TILE_HEIGHT, 0, 1);				
 		add(roads);	
 
 		roads.setTileProperties(0, FlxObject.ANY);	
-		for(i in 1...8)
+		for(i in 1...11)
 			roads.setTileProperties(i, FlxObject.NONE);			
 
+		// Cities
 		towns = new FlxTilemap();
 		towns.loadMap(towns_map, "assets/images/towns.png", TILE_WIDTH, TILE_HEIGHT, 0, 1);				
 		add(towns);	
 
+		// Buildings
+		buildings = new FlxTilemap();
+		buildings.loadMap(buildings_map, "assets/images/buildings.png", TILE_WIDTH, TILE_HEIGHT, 0, 1);				
+		add(buildings);	
+
+		// City labels
 		for(i in 0...cities.length){
 			label = new FlxText(cities[i].label_coordinate_x, cities[i].label_coordinate_y);
 			label.setFormat("assets/fonts/" + font, cityTextSize, FlxColor.WHITE, "center");		
 			label.text = cities[i].name;
 			add(label);
-
 		}				
-
 	}
 
 	public function init_camera(){
@@ -255,41 +240,47 @@ class PlayState extends FlxState{
 		add(optionsButton);							
 	}	
 
-	public function init_time(){
-		time = new Time(12, "January", 1678, 100);
-		state = Running;	
-	}	
-
-	public function init_cities(){
-		cities = new Array<City>();
-
-		var city:City;
-
-		city = new City("Ann Arbor", 14, 6, [500,200,50]);
-		cities.push(city);	
-		city = new City("Atlanta", 33, 19, [400,300,50]);
-		cities.push(city);	
-		city = new City("New York", 12, 20, [1000,400,0]);
-		cities.push(city);	
-		city = new City("Phoenix", 14, 34, [800,100,25]);
-		cities.push(city);			
-	}	
-
-	public function input_keyboard(){
-	    if (FlxG.keys.justPressed.SPACE){
-			if(state == Running)
-				state = Paused;
-			else
-				state = Running;				
-	    }	
+	public function init_trucks(){
+		trucks = new Array<FlxSprite>();
+		truck_paths = new Array<FlxPath>();
 	}
 
-	public function update_time():Void{
-		var calendar_day;
-		time.update();
-		calendar_day = time.calendar_day;
-		hud.update_time(calendar_day);
-	}
+	// Initialization Aux Functions
+
+	private function get_map(_type):String{
+		var map = "";
+		var x_cor:Int;
+		var y_cor:Int;
+		for(row in 0...LEVEL_HEIGHT){
+			y_cor = row + 1;		
+			for(col in 0...LEVEL_WIDTH){
+				x_cor = col + 1;
+				var val = "";
+				if(_type == "Cities"){
+					var town_loc = false;
+					for(i in 0...cities.length){
+						if(cities[i].coordinate_x <= x_cor+1 && cities[i].coordinate_x >= x_cor-1 && cities[i].coordinate_y <= y_cor+1 && cities[i].coordinate_y >= y_cor-1)
+							town_loc = true;
+					}
+					if(town_loc == true)
+						val = "1";
+					else
+						val = "0";
+				}
+				else if(_type == "Buildings"){
+					val = "0";
+				}				
+				map = map + val;
+				if(col < LEVEL_WIDTH - 1)
+					map = map + ",";	
+				else if(row < LEVEL_HEIGHT - 1)
+					map = map + "\n";														
+			}
+		}
+		return map;
+	}		
+
+	// Update functions
 
 	public function move_camera():Void{
 		if (FlxG.keys.anyPressed(["DOWN", "S"]))
@@ -311,7 +302,139 @@ class PlayState extends FlxState{
 			cameraFocus.y = FlxG.height / 2;
 		else if (cameraFocus.y > LEVEL_HEIGHT * TILE_HEIGHT - FlxG.height / 2)
 			cameraFocus.y = LEVEL_HEIGHT * TILE_HEIGHT - FlxG.height / 2;
+	}	
+
+	public function input_keyboard(){
+	    if (FlxG.keys.justPressed.SPACE){
+			if(state == Running)
+				state = Paused;
+			else
+				state = Running;	
+			stateSwitch = true;			
+	    }	
 	}
+	
+	public function input_mouse(){
+		var x;
+		/*
+	    if (buildMode == WheatFarm){
+			if(state == Running)
+				state = Paused;
+			else
+				state = Running;				
+	    }
+	    */	
+	}	
+
+	public function update_state_dependent_items(){
+		if(stateSwitch == true){
+			if(state == Running)
+				resume_game();
+			else
+				pause_game();
+			stateSwitch = false;		
+		}
+	}
+
+	public function update_time():Void{
+		var calendar_day;
+		time.update();
+		calendar_day = time.calendar_day;
+		hud.update_time(calendar_day);
+	}
+
+	public function move_trucks(){
+		for(i in 0...trucks.length){
+			if (!truck_paths[i].finished && truck_paths[i].nodes!=null) {
+				if (truck_paths[i].angle == 0)
+					trucks[i].animation.play("up");
+				else if (truck_paths[i].angle == 180) 
+					trucks[i].animation.play("down");
+				else if (truck_paths[i].angle == 90) 
+					trucks[i].animation.play("right");
+				else if (truck_paths[i].angle == -90) 
+					trucks[i].animation.play("left");
+			} 
+			else{
+				trucks[i].animation.curAnim.curFrame = 0;
+				trucks[i].animation.curAnim.stop();
+			}
+		}		
+	}
+
+	public function remove_arrived_trucks(){
+		var truck_no:Int;
+
+		truck_no = -1;
+		for(i in 0...trucks.length){	
+			if(truck_paths[i].finished){
+				remove(trucks[i]);	
+				truck_paths[i].destroy();	
+				truck_no = i;	
+			}
+		}
+
+		if(truck_no >= 0){
+			trucks.splice(truck_no,1);
+			truck_paths.splice(truck_no,1);
+		}
+	}	
+
+	public function execute_todays_events(){
+		if(time.total_days==1)
+			buildMode = WheatFarm;
+		if(time.total_days==2)
+			ship_truck(26,34,16,6);
+		if(time.total_days==3)
+			ship_truck(30,19,15,20);
+		
+		if(time.total_days==4){
+			//hud.update_hud(get_map("Cities"));
+
+		}
+			
+	}	
+
+
+	// Update Aux Functions
+
+	public function pause_game(){
+		for(i in 0...truck_paths.length){
+			truck_paths[i].speed = 0;
+		}
+	}
+
+	public function resume_game(){
+		for(i in 0...truck_paths.length){
+			truck_paths[i].speed = truck_def_speed;
+		}	
+	}	
+
+	public function ship_truck(from_x,from_y,to_x,to_y){
+		var truck:FlxSprite;
+		var truck_path:FlxPath;
+
+		truck = new FlxSprite((from_x - 1) * TILE_WIDTH, (from_y - 1) * TILE_HEIGHT);
+		truck.loadGraphic("assets/images/truck.png", true, TILE_WIDTH, TILE_HEIGHT);
+		truck.animation.add("left", [0]);	
+		truck.animation.add("right", [1]);			
+		truck.animation.add("down", [2]);
+		truck.animation.add("up", [3]);		
+		
+		trucks.push(truck);
+
+		add(trucks[trucks.length - 1]);
+
+		truck_path = new FlxPath();	
+		//truck_path.speed = 10;
+		truck_paths.push(truck_path);	
+	
+		var nodes:Array<FlxPoint> = roads.findPath(FlxPoint.get(truck.x, truck.y), FlxPoint.get((to_x - 0.5) * TILE_WIDTH, (to_y - 0.5) * TILE_HEIGHT));
+
+		truck_paths[truck_paths.length - 1].start(trucks[trucks.length - 1], nodes, truck_def_speed);
+	}	
+
+	// Menu handling
 
 	private function goCompany():Void {
 		FlxG.switchState(new MenuState());
@@ -323,32 +446,6 @@ class PlayState extends FlxState{
 
 	private function goOptions():Void {
 		FlxG.switchState(new MenuState());
-	}		
-
-	private function get_cities_map():String{
-		var map = "";
-		var x_cor:Int;
-		var y_cor:Int;
-		for(row in 0...LEVEL_HEIGHT){
-			y_cor = row + 1;		
-			for(col in 0...LEVEL_WIDTH){
-				var town_loc = false;
-				x_cor = col + 1;
-				for(i in 0...cities.length){
-					if(cities[i].coordinate_x <= x_cor+1 && cities[i].coordinate_x >= x_cor-1 && cities[i].coordinate_y <= y_cor+1 && cities[i].coordinate_y >= y_cor-1)
-						town_loc = true;
-				}
-				if(town_loc == true)
-					map = map + "1";
-				else
-					map = map + "0";	
-				if(col < LEVEL_WIDTH - 1)
-					map = map + ",";	
-				else if(row < LEVEL_HEIGHT - 1)
-					map = map + "\n";														
-			}
-		}
-		return map;
-	}				
+	}					
 
 }
